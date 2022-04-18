af9859439a3597512b4c91846d7930c4b43044b2
robsonjb1 <jason@jasonrobson.com>
2022-04-18 10:38:12 +0100

    Previous assumption on MemoryCache incorrect

--- API/AppCode/Magazine/CarouselCache.cs
+++ API/AppCode/Magazine/CarouselCache.cs
@@ -1,38 +1,44 @@
-﻿using System.Text;
+﻿using System;
+using System.Text;
+using System.Web;
 using Microsoft.Extensions.Caching.Memory;
 
 namespace PagesFromCeefax
 {
     public class CarouselCache
     {
-        private string _currentCarousel = String.Empty;
-        private DateTime _lastBuilt = DateTime.Now.AddYears(-1);
+        private readonly IMemoryCache _currentCarousel = new MemoryCache(new MemoryCacheOptions());
+
         private Object l = new Object();
         private int _totalRequests = 0;
         private int _totalCarousels = 0;
         private DateTime _serviceStart = DateTime.Now;
-    
+        private DateTime _lastBuilt = DateTime.Now;
+
         public string GetMagazine(ILogger logger)
         {
             // Only refresh the magazine on the first get (not on service start)
             _totalRequests++;
             lock (l)
             {
-                if (DateTime.Now > _lastBuilt.AddMinutes(20))
-                { 
+                var content = _currentCarousel.Get<string>("carousel");
+                if (content is null)
+                {
                     _totalCarousels++;
 
                     DateTime start = DateTime.Now;
-                    CarouselBuilder cb = new CarouselBuilder();
-                    _currentCarousel = cb.Content.DisplayHtml.ToString();
-                    DateTime end = _lastBuilt = DateTime.Now;
+                    var c = new CarouselBuilder();
+                    _lastBuilt = DateTime.Now;
 
-                    logger.LogInformation($"Generated new carousel #{_totalCarousels} in {(end - start).TotalMilliseconds}ms");
+                    content = c.Content.DisplayHtml.ToString();
+                    _currentCarousel.Set("carousel", content, TimeSpan.FromMinutes(30));
+                 
+                    logger.LogInformation($"Generated new carousel #{_totalCarousels} in {(_lastBuilt - start).TotalMilliseconds}ms");
                 }
 
                 logger.LogInformation($"Returning carousel request {_totalRequests}");
 
-                return _currentCarousel
+                return content!
                     .Replace("{PFC_TOTALREQUESTS}", _totalRequests.ToString())
                     .Replace("{PFC_TOTALCAROUSELS}", _totalCarousels.ToString())
                     .Replace("{PFC_SERVICESTART}", _serviceStart.DayOfWeek.ToString().Substring(0, 3) + _serviceStart.ToString(" dd MMM HH:mm/ss"))
@@ -41,4 +47,3 @@ namespace PagesFromCeefax
         }
     }
 }
-
