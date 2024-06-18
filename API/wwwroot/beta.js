9518774250910b53052ac049bc8e6c3ac068e223
Jason Robson <jason@jasonrobson.com>
2024-06-18 19:46:21 +0100

    beta

--- API/wwwroot/beta.js
+++ API/wwwroot/beta.js
@@ -13,27 +13,101 @@ const observer = new ResizeObserver((entries) => {
 });
 observer.observe(canvas);
 
-// not import but draw something just to showcase
 
 function render(time) {
-  canvas.width = width;
-  canvas.height = height;
+  canvas.width = 480; //width;
+  canvas.height = 480; //height;
 
-  ctx.save();
+  var imgData = ctx.createImageData(480, 480);
+
+  var idPtr = 0;
+
+  for(var yPos = 0; yPos < 24 * 20; yPos++)
+  {
+    for(var xPos = 0; xPos < 40 * 12; xPos++)
+    {
+        imgData.data[idPtr] = charArray[((12 * yPos) % 240) + (xPos % 12)] == 1 ? 255 : 0;
+        imgData.data[idPtr+1] = charArray[((12 * yPos) % 240) + (xPos % 12)] == 1 ? 255 : 0;
+        imgData.data[idPtr+2] = charArray[((12 * yPos) % 240) + (xPos % 12)] == 1 ? 255 : 0;
+        imgData.data[idPtr+3] = 255;
+        idPtr = idPtr + 4;
+    }
+  }
+
+  ctx.putImageData(imgData, 0, 0);
+
+  /*
+  var startX = 50;
+  var startY = 50;
+  var posX = startX;
+  var posY = startY;
+
+  for (var charPos = 0; charPos < 240; charPos++)
+  {
+    if(charPos % 12 == 0)
+    {
+      posY++;
+      posX = startX;
+    }
+     
+
+    if(charArray[charPos] == 1)
+    {
+      ctx.save();
+      ctx.beginPath();
+      ctx.strokeStyle = '#f0f0f0';
+      ctx.lineWidth = 1;
+      ctx.rect(posX, posY, 1, 1);
+      ctx.stroke();
+      ctx.restore();
+      posX++;
+    }
+
+    if(charArray[charPos] == 0)
+    {
+      /*
+      ctx.save();
+      ctx.beginPath();
+      ctx.strokeStyle = '#0000f0';
+      ctx.lineWidth = 1;
+      ctx.rect(posX, posY, 1, 1);
+      ctx.stroke();
+      ctx.restore();
+   
+  */
   
-  ctx.beginPath();
-  ctx.strokeStyle = '#000000';
-  ctx.lineWidth = 10;
-  ctx.rect(20, 20, width-40, height-40);
-  ctx.stroke();
+  
 
-  ctx.beginPath();
-  ctx.strokeStyle = '#ff0000';
-  ctx.lineWidth = 10;
-  ctx.rect(50, 50, width-100, height-100);
-  ctx.stroke();
-  ctx.restore();
-
+  
   requestAnimationFrame(render);
 }
 requestAnimationFrame(render);
+
+function makeChars() {
+    // prettier-ignore
+    return new Uint8Array([
+        // 'a'
+        0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0,
+        0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0,
+        0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0,
+        0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0,
+        0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0,
+        0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0,
+        0, 0, 0, 0, 1, 1, 1, 1, 1, 1, 0, 0,
+        0, 0, 0, 0, 1, 1, 1, 1, 1, 1, 1, 0,
+        0, 0, 0, 0, 0, 0, 0, 0, 0, 1, 1, 1,
+        0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 1, 1,
+        0, 0, 0, 0, 1, 1, 1, 1, 1, 1, 1, 1,
+        0, 0, 0, 1, 1, 1, 1, 1, 1, 1, 1, 1,
+        0, 0, 1, 1, 1, 0, 0, 0, 0, 0, 1, 1,
+        0, 0, 1, 1, 1, 0, 0, 0, 0, 0, 1, 1,
+        0, 0, 0, 1, 1, 1, 1, 1, 1, 1, 1, 1,
+        0, 0, 0, 0, 1, 1, 1, 1, 1, 1, 1, 1,
+        0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0,
+        0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0,
+        0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0,
+        0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0
+    ]);
+}
+
+var charArray = makeChars();
