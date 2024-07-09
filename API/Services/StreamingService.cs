3ccccdeb4d28aa0519b40bf4d60c1a2bb2b13dc1
Jason Robson <jason@jasonrobson.com>
2024-07-09 09:13:57 +0100

    service list update

--- API/Services/StreamingService.cs
+++ API/Services/StreamingService.cs
@@ -1,39 +1,9 @@
-
-
 using API.Architecture;
 
 public interface IStreamingService
 {
     public string StreamChannel(string channel);
 }
-
-// 1-1 The Way Back
-// https://onedrive.live.com/embed?resid=6B5874908501F6E4%211026632&authkey=!AJioJZNBH-LeSkQ
-
-// 1-2 Space Fall
-// https://onedrive.live.com/embed?resid=6B5874908501F6E4%211026640&authkey=!ABSBTXNSxJsJBjA
-
-// 1-3 Cygnus Alpha
-// https://onedrive.live.com/embed?resid=6B5874908501F6E4%211026642&authkey=!AOtW963EiDftwT4
-
-// 1-4 Time Squad
-// https://onedrive.live.com/embed?resid=6B5874908501F6E4%211026641&authkey=!ABWKQ8xgUIGuzFg
-
-// 1-5 The Web
-// https://onedrive.live.com/embed?resid=6B5874908501F6E4%211026643&authkey=!AH-VmWK_dm_Jpt0
-
-// 1-6 Seek, Locate, Destroy
-// https://onedrive.live.com/embed?resid=6B5874908501F6E4%211026645&authkey=!ALWgTs15ki1XJ1E
-
-// 1-7 Mission to Destiny
-// https://onedrive.live.com/embed?resid=6B5874908501F6E4%211026644&authkey=!AFq61TOifbFDwNo
-
-// 1-8 Duel
-// https://onedrive.live.com/embed?resid=6B5874908501F6E4%211026646&authkey=!ANcHcbOEygyp-hs
-
-// 1-9 Project Avalon
-// https://onedrive.live.com/embed?resid=6B5874908501F6E4%211026648&authkey=!AOzR7vEul49NO54
-
 
 public class StreamingService : IStreamingService
 {
@@ -52,7 +22,7 @@ public class StreamingService : IStreamingService
 
     public StreamingService()
     {
-        string listings = File.ReadAllText("./wwwroot/tv/listings.dat");
+        string listings = File.ReadAllText("./wwwroot/tv/listings.txt");
         bool first = true;
         string name = String.Empty;
         foreach (string entry in listings.Split(Environment.NewLine))
