7e53cc3e5a5f18c01693582c3da9899f0cb36219
Jason Robson <jason@jasonrobson.com>
2024-08-25 12:07:49 +0100

    Update Graphics.cs

--- API/Architecture/Graphics.cs
+++ API/Architecture/Graphics.cs
@@ -152,7 +152,7 @@ public class Graphics
         PromoMap.AppendLine($"[LINE5][{TeletextControl.GraphicsGreen}][{TeletextControl.SeparatedGraphics}]  ~o¬¬¬¬w1");
         PromoMap.AppendLine($"[LINE6][{TeletextControl.GraphicsGreen}][{TeletextControl.SeparatedGraphics}]  ~o¬¬¬¬wq");
         PromoMap.AppendLine($"[LINE7][{TeletextControl.GraphicsGreen}][{TeletextControl.SeparatedGraphics}] j)\"¬[DD]¬}}");
-        PromoMap.AppendLine($"[{TeletextControl.GraphicsGreen}][{TeletextControl.SeparatedGraphics}]                p|¬t  x¬¬g¬¬¬¬0");
+        PromoMap.AppendLine($"[{TeletextControl.GraphicsGreen}][{TeletextControl.SeparatedGraphics}]                 p|¬t  x¬¬g¬¬¬¬0");
         PromoMap.AppendLine($"[{TeletextControl.AlphaWhite}]Current[{TeletextControl.GraphicsGreen}][{TeletextControl.SeparatedGraphics}]        n[EE]t +\" ¬¬¬¬¬¬");
         PromoMap.AppendLine($"[{TeletextControl.AlphaWhite}]temperatures[{TeletextControl.GraphicsGreen}][{TeletextControl.SeparatedGraphics}]   +¬¬¬¬¬ _4 +¬¬¬¬¬|0");
         PromoMap.AppendLine($"[{TeletextControl.AlphaWhite}]at [TTT][{TeletextControl.GraphicsGreen}][{TeletextControl.SeparatedGraphics}]        \"%*o! \"   *¬¬¬¬¬u");
